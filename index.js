5af356823f75908c401a8c809befdf541b03080f
dan <dang240@gmail.com>
2023-07-29 12:06:26 +0300

    adding

---
+++ index.js
@@ -0,0 +1,18 @@
+import 'react-native-gesture-handler'; // Import this line at the top of the file
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+import { GestureHandlerRootView } from 'react-native-gesture-handler'; // Import GestureHandlerRootView
+
+// Enable React Native Web
+if (window.document) {
+  AppRegistry.registerComponent(appName, () => () => (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <App />
+    </GestureHandlerRootView>
+  ));
+  AppRegistry.runApplication(appName, { initialProps: {}, rootTag: document.getElementById('app-root') });
+}
+ 
+
+// check
